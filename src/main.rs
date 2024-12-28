55f143b59493b8e79dc60275c891469541e75ec4
Rohan Kapur <rohankapurdev@outlook.com>
2024-12-28 11:21:59 +0800

    comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //! The tests here are all for shared memory spsc queues sending messages between threads. I don't
-//! care about process private memory, so I'm not going to test that. This uses the RDTSC register
-//! to measure the time it takes to send messages between threads.
+//! care about process private memory, so I'm not going to test that
 
 use message::IpcMessage;
 use std::thread;
@@ -8,7 +7,9 @@ use std::thread;
 mod message;
 
 fn main() {
-    let messages = IpcMessage::create_dummy_messages(10000);
+    // Preallocate the messages
+    let small_messages = IpcMessage::create_dummy_messages(10000);
+    let medium_messages = IpcMessage::create_dummy_messages(100000);
 }
 
 fn test_temporal_que() {
